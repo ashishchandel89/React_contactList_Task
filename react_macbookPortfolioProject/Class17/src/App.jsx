1534504b697aab44f5ce589fc414c4c07d10c23a
ashishchandel89 <chandel078ashish@gmail.com>
2026-08-03 17:07:55 +0530

    task-bar complete

---
+++ react_macbookPortfolioProject/Class17/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './app.scss'
+import Dock from './components/Dock'
+
+const App = () => {
+  return (
+    
+      <main>
+        <div>
+          <Dock/>
+        </div>
+      </main>
+   
+  )
+}
+
+export default App
